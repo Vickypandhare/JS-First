4d283a1113d360b7ab6c17498b6ab783bbf1226e
Vickypandhare <146652743+Vickypandhare@users.noreply.github.com>
2024-09-30 08:33:50 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "vickypandhare@gmail.com"
+var accountPassword = "Pass@123"
+let accountState;
+
+accountEmail = "rutuja@gmail.com"
+accountPassword = "rutuja222"
+accountCity = "pune"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,3 @@
+//number => 2 to the power 23
+//bigInt 
+//
