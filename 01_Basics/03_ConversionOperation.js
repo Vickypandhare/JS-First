a052a30995aea3ebf9aa7b17db833ef4648139fb
Vickypandhare <146652743+Vickypandhare@users.noreply.github.com>
2024-10-01 20:08:15 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = "Rutuja"
+
+// console.log(score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Vicky"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false;
+// "" => false
+// "rutuja" => true 
+
+let someNumber = 79
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
