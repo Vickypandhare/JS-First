253bf59106c83e3f9e8e3f4f32fd7ce3329bde09
Vickypandhare <146652743+Vickypandhare@users.noreply.github.com>
2024-10-01 07:30:54 +0000

    DataTypes

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,3 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+///alert()
+
+console.log(3 + 3);
+
+let name = "Vicky"
+let age = 23
+let isLoggedIn = true
+
 //number => 2 to the power 23
 //bigInt 
-//
+//string => ""
+//boolean => ture/false
+//null => stand alone value
+//undefined => undefined
+//Symbol => unique
+
+//Object
+
+console.log(typeof undefined); //undefined
+console.log(typeof age);
+console.log(typeof null); //Object
+
+
+
